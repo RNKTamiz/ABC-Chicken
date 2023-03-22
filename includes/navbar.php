b97164b2f8e4086b92fb92b521673a6c29a521e9
RNKTamiz <118505055+RNKTamiz@users.noreply.github.com>
2023-03-22 10:36:03 +0530

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,7 +10,7 @@
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="categories">Collections</a>
+          <a class="nav-link" href="categories.php">Collections</a>
         </li>
 
         <?php
