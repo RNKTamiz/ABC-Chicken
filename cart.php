809ae429251edaebd19679f0049525197fd5b753
RNKTamiz <118505055+RNKTamiz@users.noreply.github.com>
2023-03-24 15:33:46 +0530

    er

--- cart.php
+++ cart.php
@@ -26,7 +26,7 @@
             <div class="row">
                 <div class="col-md-12">
 
-                    <div class="row align-items-center mt-3">
+                    <!-- <div class="row align-items-center mt-3">
                         <div class="col-md-5">
                             <h5>Product</h5>                            
                         </div>
@@ -39,7 +39,7 @@
                         <div class="col-md-2">
                             <h5>Remove</h5>
                         </div>
-                    </div>
+                    </div> -->
 
                     <?php
                         $items = getCartItems();
@@ -50,15 +50,19 @@
                             <div class="card product-data shadow-sm mb-3">
                                 <div class="row align-items-center mt-3">
                                     <div class="col-md-2">
+                                        <h5><b>Product Image</b></h5>
                                         <img src="uploads/<?= $citem['image'];?>" alt="image" width="80px" class="mb-3 ml-3">
                                     </div>
                                     <div class="col-md-3">
+                                        <h5><b>Product Name</b></h5>
                                         <h6><?= $citem['name'];?></h6>                            
                                     </div>
                                     <div class="col-md-3">
+                                        <h5><b>Price</b></h5>
                                         <h6>Rs <?= $citem['selling_price'];?></h6>                            
                                     </div>
                                     <div class="col-md-2">
+                                        <h5><b>Quantity</b></h5>
                                         <div class="input-group mb-3" style="width:130px">
                                             <button class="input-group-text decrement-btn">-</button>
                                             <input type="text" class="form-control bg-white text-center input-qty" value="<?= $citem['prod_qty'];?>" disabled>
@@ -66,6 +70,7 @@
                                         </div>                         
                                     </div>
                                     <div class="col-md-2">
+                                        <h5><b>Remove</b></h5>
                                         <button class="btn btn-danger btn-sm"><i class="fa fa-trash mr-2"></i>Remove</button>
                                     </div>
                                 </div>
